6af7e26393ea11363078b8de3347307ce07634aa
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-26 04:35:36 -0800

    refact: moved CLI arguments struct to `lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser)]
+/// CLI Argument Struct
+/// Automatically derived into parsing structs by clap
+pub struct CliArgs {
+    ///Pattern to look for
+    pub pattern: String,
+    ///path to file; for reading
+    pub path: std::path::PathBuf,
+}
+
+/////////////////////////////////////
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
 //! executable code for a clap / cli learning tutorial
 
 use clap::Parser;
-
-#[derive(Parser)]
-/// CLI Argument Struct\n
-/// Automatically derived into parsing structs by clap
-struct Cli {
-    ///Pattern to look for
-    pattern: String,
-    ///path to file; for reading
-    path: std::path::PathBuf,
-}
+use clap_acq::CliArgs;
 
 fn main() {
-    let args = Cli::parse();
+    let args = CliArgs::parse();
 
     println!("Pattern: {}", args.pattern);
     println!("Path: {}", args.path.display());
