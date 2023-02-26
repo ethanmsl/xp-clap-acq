896ec30fd569795185e9915674cc2101340dc42e
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-26 07:58:33 -0800

    feat: primitive grepping

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,20 @@ use clap_acq::CliArgs;
 
 fn main() {
     let args = CliArgs::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read string");
 
     println!("Pattern: {}", args.pattern);
     println!("Path: {}", args.path.display());
+
+    let mut matchfound = false;
+    println!("\nSearching for matches...:");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            matchfound = true;
+            println!("{}", line);
+        }
+    }
+    if !matchfound {
+        println!("No match found");
+    }
 }
