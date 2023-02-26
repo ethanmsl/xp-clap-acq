ec20816ecec52d83ab1e11b654fa72ecbfa60a9f
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-26 03:22:54 -0800

    feat: input struct and basic use thereof

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 //! executable code for a clap / cli learning tutorial
 
+use clap::Parser;
+
+#[derive(Parser)]
+struct CLI {
+    ///Pattern to look for
+    pattern: String,
+    ///path to file; for reading
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let args = CLI::parse();
+
+    println!("Pattern: {}", args.pattern);
+    println!("Path: {}", args.path.display());
 }
