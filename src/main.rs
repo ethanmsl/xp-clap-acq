d5f3967cd6a95cb3cc04ebd9d0bb85edf4070fd9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-26 08:12:57 -0800

    refact: using buffered stream instead of moving whole file to memory

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,26 @@
 
 use clap::Parser;
 use clap_acq::CliArgs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
     let args = CliArgs::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read string");
+    let file = File::open(&args.path).expect("could not open file");
+    let reader = BufReader::new(file);
+    // let content = std::fs::read_to_string(&args.path).expect("could not read string");
+    // let what = reader.lines();
 
     println!("Pattern: {}", args.pattern);
     println!("Path: {}", args.path.display());
 
     let mut matchfound = false;
+
     println!("\nSearching for matches...:");
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
+    for line in reader.lines() {
+        if line.as_ref().unwrap().contains(&args.pattern) {
             matchfound = true;
-            println!("{}", line);
+            println!("{:?}", line);
         }
     }
     if !matchfound {
